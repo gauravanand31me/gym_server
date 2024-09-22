a069f6dbd7e3a4de2aff0556f3eb4fd6021c14ca
Sourav Satyam <sourav.satyamsingh@gmail.com>
2024-09-22 18:36:00 +0530

    ADded gym api route request

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-app.use('/api', routes);
+app.use('/gym/api', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

42ddae2d433f0fe8f121b7c4627495f57ff252f5
Sourav Satyam <sourav.satyamsingh@gmail.com>
2024-09-22 13:49:41 +0530

    Added process.env

--- models/index.js
+++ models/index.js
@@ -9,6 +9,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 console.log("config", config);
+console.log("process.env", process.env);
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);

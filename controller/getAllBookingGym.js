e2b8427ac8f2b724b58b5d5c10067be6869b322a
Sourav Satyam <sourav.satyamsingh@gmail.com>
2024-10-27 19:30:53 +0530

    Adeded sequalize

--- controller/getAllBookingGym.js
+++ controller/getAllBookingGym.js
@@ -1,5 +1,6 @@
 const JWT_SECRET = process.env.JWT_SECRET || 'Testing@123';
 const jwt = require('jsonwebtoken'); // Import jsonwebtoken
+const sequelize = require("sequelize");
 exports.getAllBookingsToGym = async (req, res) => {
     try {
 

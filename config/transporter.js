2d2d22407d403c4a444ec66169d29df574a2974b
Sourav Satyam <sourav.satyamsingh@gmail.com>
2024-11-03 23:15:07 +0530

    Some changes to transporter

--- config/transporter.js
+++ config/transporter.js
@@ -1,20 +1,26 @@
 const nodemailer = require('nodemailer');
-
+console.log("process.env.GODADDY_EMAIL", process.env.GODADDY_EMAIL);
+console.log("process.env.GODADDY_PASS", process.env.GODADDY_PASS);
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+service: 'Godaddy',
+  host: 'smtpout.secureserver.net', // GoDaddy SMTP server
+  port: 465, // Use 465 for SSL or 587 for TLS
+  secure: true, // true for 465, false for 587
   auth: {
-    user: process.env.GMAIL_EMAIL,  // Your Gmail email address
-    pass: process.env.GMAIL_PASS    // Your Gmail app-specific password
+    user: process.env.GODADDY_EMAIL, // GoDaddy email address
+    pass: process.env.GODADDY_PASS // GoDaddy email password
   }
 });
 
 // Verify the connection configuration
 transporter.verify(function(error, success) {
+    console.log("error", error);
+    console.log("Success", success);
   if (error) {
     console.log('Error connecting to email server:', error);
   } else {
     console.log('Server is ready to take messages');
   }
-});
+})
 
 module.exports = transporter;

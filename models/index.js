e6bf208a57b8e199e0972d8e90a6c9e4cd3f0589
Sourav Satyam <sourav.satyamsingh@gmail.com>
2025-01-02 14:57:40 +0530

    Added the dialets options

--- models/index.js
+++ models/index.js
@@ -8,13 +8,27 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
+
 console.log("config", config);
 console.log("process.env", process.env);
+
 let sequelize;
+
+// Add dialectOptions for SSL if required (for AWS RDS)
+const dialectOptions = {
+  ssl: {
+    require: true, // This will help you. But you will see nwe error
+    rejectUnauthorized: false // This line will fix new error
+  }
+};
+
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    ...config, // Spread the existing config properties
+    dialectOptions, // Add the dialect options here
+  });
 }
 
 fs

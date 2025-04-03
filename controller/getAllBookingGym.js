b4edc5212ad73b05e0ba7bf810d2487ab6b25d5f
Gourav Anand <Gauravanand32me@gmail.com>
2025-04-03 11:07:59 +0530

    Added unique customers for gym

--- controller/getAllBookingGym.js
+++ controller/getAllBookingGym.js
@@ -58,3 +58,56 @@ exports.getAllBookingsToGym = async (req, res) => {
       res.status(500).send('Server error');
     }
   };
+
+
+  exports.getUniqueUsersWithLastBooking = async (req, res) => {
+    try {
+      const token = req.headers['auth']; // Get JWT token from headers
+  
+      if (!token) {
+        return res.status(401).json({ error: 'No token provided' });
+      }
+  
+      const decoded = jwt.verify(token, JWT_SECRET);
+  
+      if (!decoded || !decoded.id) {
+        return res.status(401).json({ error: 'Invalid token' });
+      }
+  
+      const gymId = decoded.id;
+  
+      const query = `
+        SELECT DISTINCT ON ("Booking"."userId")
+          "Booking"."userId" AS "userId",
+          "Users".full_name AS "userFullName",
+          "Users".mobile_number AS "userMobileNumber",
+          "Booking"."bookingId",
+          "Booking"."bookingDate",
+          "Booking"."isCheckedIn" AS "visited",
+          "Booking".price AS "subscriptionPrice",
+          "Booking"."isPaid" AS "isPaid",
+          "Booking"."type" AS "bookingType",
+          "Booking"."createdAt" AS "create",
+          "Slots"."startTime" AS "slotStartTime",
+          "Gyms".id AS "gymId",
+          "Gyms".name AS "gymName",
+          "Gyms".rating AS "gymRating"
+        FROM "Booking"
+        JOIN "Slots" ON "Booking"."slotId" = "Slots".id
+        JOIN "Users" ON "Users"."id" = "Booking"."userId"
+        JOIN "Gyms" ON "Slots"."gymId" = "Gyms".id
+        WHERE "Gyms".id = :gymId
+        ORDER BY "Booking"."userId", "Booking"."createdAt" DESC
+      `;
+  
+      const [results] = await sequelize.query(query, {
+        replacements: { gymId },
+      });
+  
+      res.status(200).json({ UniqueCustomers: results });
+    } catch (error) {
+      console.error('Error fetching unique users:', error);
+      res.status(500).send('Server error');
+    }
+  };
+  

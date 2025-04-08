d64bb1f678e2c26abd2c1aad39a2e1bb8630c619
Gourav Anand <Gauravanand32me@gmail.com>
2025-04-08 18:15:15 +0530

    Added a model for gym coupons.

---
+++ models/gymcoupons.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+    const CouponGymMap = sequelize.define('CouponGymMap', {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+      },
+      coupon_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+          model: 'coupons',
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      gym_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+          model: 'Gyms', // or 'gyms' depending on your table name
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false
+      }
+    }, {
+      tableName: 'coupon_gyms',
+      timestamps: false // disable default camelCase timestamps
+    });
+  
+    return CouponGymMap;
+  };
+  

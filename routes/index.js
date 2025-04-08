5e31611ec998882ee6edb759b02d200af9f84b25
Gourav Anand <Gauravanand32me@gmail.com>
2025-04-08 17:52:58 +0530

    Passing empty find all query

--- routes/index.js
+++ routes/index.js
@@ -132,9 +132,7 @@ router.get("/admin/coupons", requireAdmin, async (req, res) => {
     );
 
     // Fetch all coupons
-    const coupons = await Coupon.findAll({
-      order: [['created_at', 'DESC']]
-    });
+    const coupons = await Coupon.findAll({});
 
     // Render to a Pug template
     res.render("admin-coupons", { coupons, token });

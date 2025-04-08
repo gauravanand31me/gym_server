6e2cbaad40da4a49ef00bf4103e29d5498301ff6
Gourav Anand <Gauravanand32me@gmail.com>
2025-04-08 17:51:16 +0530

    Added created_at

--- routes/index.js
+++ routes/index.js
@@ -132,7 +132,9 @@ router.get("/admin/coupons", requireAdmin, async (req, res) => {
     );
 
     // Fetch all coupons
-    const coupons = await Coupon.findAll({ order: [['created_at', 'DESC']] });
+    const coupons = await Coupon.findAll({
+      order: [['created_at', 'DESC']]
+    });
 
     // Render to a Pug template
     res.render("admin-coupons", { coupons, token });

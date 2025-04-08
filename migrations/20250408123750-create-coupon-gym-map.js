8129add8d282c2236f5325728ec333057d1a301a
Gourav Anand <Gauravanand32me@gmail.com>
2025-04-08 18:10:07 +0530

    Added the default value to the coupon code

--- migrations/20250408123750-create-coupon-gym-map.js
+++ migrations/20250408123750-create-coupon-gym-map.js
@@ -5,7 +5,7 @@ module.exports = {
     await queryInterface.createTable('coupon_gyms', {
       id: {
         type: Sequelize.UUID,
-        defaultValue: Sequelize.literal('uuid_generate_v4()'),
+        defaultValue: Sequelize.UUIDV4,
         primaryKey: true,
       },
       coupon_id: {
